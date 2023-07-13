89ffb2e16f82a4eb5959602cacbc9f1cfe38b015
aidcom5894 <vivekrobinkujur_jsr@hotmail.com>
2023-07-14 02:13:35 +0530

    updated

--- application/config/config.php
+++ application/config/config.php
@@ -23,11 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-<<<<<<< HEAD
-$config['base_url'] = 'https://interns.aidcom.in/';
-=======
 $config['base_url'] = 'http://localhost/grc_interns/';
->>>>>>> main
 
 /*
 |--------------------------------------------------------------------------
