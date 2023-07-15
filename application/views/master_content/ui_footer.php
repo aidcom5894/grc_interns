3bdfaca83377643474008204c384044e811d59f4
aidcom5894 <vivekrobinkujur_jsr@hotmail.com>
2023-07-15 17:18:02 +0530

    updated ui and dashboard theme with signup and login

---
+++ application/views/master_content/ui_footer.php
@@ -0,0 +1,320 @@
+
+<!---footer start--->
+<footer class="footer style_four">
+<!---============spacing==========--->
+<div class="pd_top_80"></div>
+<!---============spacing==========--->
+<section class="container">
+<div class="call_to_action style_one">
+<div class="section_title type_one">
+<div class="title_whole">
+<h4 class="sm_title"> Contact Us</h4>
+<h2 class="title">
+Find an Insurance Agent or Start
+Tracking your Claims
+</h2>
+</div>
+</div>
+<div class="theme_btn_all">
+<a href="#" class="theme_btn">
+Track Your Claim <span> <i class=" fi-rr-arrow-small-up"></i></span>
+</a>
+</div>
+<div class="d-flex align-items-center contact_header_one">
+<div class="icon_s">
+<i class=" fi-rr-headphones"></i>
+</div>
+<div class="content">
+<h6 class="tite">Need Help?</h6>
+<div class="title_20"><a href="tel:+000(123)45688">+000(123)456 88</a></div>
+</div>
+</div>
+</div>
+
+</section>
+<!---============spacing==========--->
+<div class="pd_bottom_50"></div>
+<!---============spacing==========--->
+<section class="mid_content">
+<div class="container">
+<div class="row">
+<div class="col-lg-12">
+<ul class="list_box text-center linline">
+<li>
+<div class="d-flex align-items-center">
+<div class="icon trans"></div>
+<a class="links color_white" href="#">
+Home </a>
+</div>
+</li>
+<li>
+<div class="d-flex align-items-center">
+<div class="icon trans"></div>
+<a class="links color_white" href="#">
+About </a>
+</div>
+</li>
+<li>
+<div class="d-flex align-items-center">
+<div class="icon trans"></div>
+<a class="links color_white" href="#">
+Services </a>
+</div>
+</li>
+<li>
+<div class="d-flex align-items-center">
+<div class="icon trans"></div>
+<a class="links color_white" href="#">
+Reviews </a>
+</div>
+</li>
+<li>
+<div class="d-flex align-items-center">
+<div class="icon trans"></div>
+<a class="links color_white" href="#">
+Need a Career? </a>
+</div>
+</li>
+<li>
+<div class="d-flex align-items-center">
+<div class="icon trans"></div>
+<a class="links color_white" href="#">
+News & Blog </a>
+</div>
+</li>
+<li>
+<div class="d-flex align-items-center">
+<div class="icon trans"></div>
+<a class="links color_white" href="#">
+Support </a>
+</div>
+</li>
+</ul>
+</div>
+</div>
+</div>
+</section>
+<!---============spacing==========--->
+<div class="pd_bottom_50"></div>
+<!---============spacing==========--->
+<section class="container">
+<div class="bottom_content">
+<div class="row">
+<div class="col-lg-5">
+<div class="logo_box">
+<a href="#" class="logo navbar-brand">
+<img src="<?php echo base_url(); ?>modules/uiDesign/assets/images/footer-logo-three.png" alt="Vankine" class="logo_default">
+</a>
+</div>
+</div>
+<div class="col-lg-7 text-md-end">
+<div class="position-relative color_white">
+Copyright 2023, Vankine. All Rights Reserved</div>
+</div>
+</div>
+
+</div>
+</section>
+<!---============spacing==========--->
+<div class="pd_bottom_80"></div>
+<!---============spacing==========--->
+<div class="pattern_1">
+<img src="<?php echo base_url(); ?>modules/uiDesign/assets/images/dot-round-2.png" class="img-fluid" alt="img" />
+</div>
+<div class="pattern_2">
+<img src="<?php echo base_url(); ?>modules/uiDesign/assets/images/pattern-3.png" class="img-fluid" alt="img" />
+</div>
+<div class="pattern_3">
+<img src="<?php echo base_url(); ?>modules/uiDesign/assets/images/h-6-shap1-2.png" class="img-fluid" alt="img" />
+</div>
+</footer>
+<!--footer end--->
+<!----==============mobile_menu_box ==============--->
+<div class="mobile_menu_box">
+<div class="menu-backdrop"></div>
+<nav class="menu-box scrollbarcolor">
+<div class="close-btn"><i class="fi-rr-cross"></i></div>
+<form role="search" method="get" action="#">
+<input type="search" class="search" placeholder="Search..." value="" name="s" title="Search" />
+<button type="submit" class="sch_btn"> <i class="fa fa-search"></i></button>
+</form>
+<div class="menu-outer">
+<div class="navigation_menu">
+<ul class="navbar_nav getmobilemenu">
+
+</ul>
+</div>
+</div>
+<div class="mobile-header-info-wrap">
+<div class="single-mobile-header-info">
+<a href="tel:9806071234" class="cnt"><i class="fi-rs-headphones"></i>9806071234</a>
+</div>
+
+<div class="single-mobile-header-info cont_over">
+<a href="mailto:sendmail@example.com" class="cnt"><i
+class="fi-rs-envelope"></i>sendmail@example.com</a>
+</div>
+<div class="single-mobile-header-info">
+<a href="#" class="theme_btn tp_one">Contact</a>
+</div>
+<div class="single-mobile-header-info">
+<a href="#" class="theme_btn">Service</a>
+</div>
+</div>
+
+</nav>
+</div>
+<!----==============mobile_menu_box end==============--->
+<!----==============search-popup==============--->
+<div class="search-popup">
+<div class="close-search zoom-in-zoom-out"><i class="fa fa-times"></i></div>
+<div class="popup-inner">
+<div class="overlay-layer"></div>
+<div class="search-form">
+<fieldset>
+<form role="search" method="get" action="#">
+<input type="search" class="search" placeholder="Search..." value="" name="s"
+title="Search" />
+<button type="submit" class="sch_btn"> <i class="fi-rs-search"></i></button>
+</form>
+</fieldset>
+</div>
+</div>
+</div>
+<!----==============search-popup end==============--->
+<!----==============option_panel==============--->
+<div class="option_panel-popup">
+<div class="sdmenu_overlay"></div>
+<div class="option_boxed scrollbarcolor">
+<div class="close-option-pan">
+<i class="fi-rr-cross"></i>
+</div>
+<div class="option_content scrollbarcolor ">
+<div class="log_bx">
+<a href="#" class="logo navbar-brand">
+<img src="<?php echo base_url(); ?>modules/uiDesign/assets/images/theme-logo.png" alt="Vankine" class="logo_default">
+</a>
+<div class="about_company">
+Denounce with righteous indignation and dislike men who are beguiled
+and demoralized by the charms pleasure moment so blinded desire that
+they cannot foresee the pain and trouble.
+</div>
+</div>
+<div class="contnet_box">
+<div class="post_contet_modal">
+<h2 class="title_no_a_28"> Latest Portfolio</h2>
+<div class="post_enable">
+<div class="theme_carousel owl-theme owl-carousel"
+data-options='{"loop": true, "margin": 10,  "autoheight":true, "lazyload":true, "nav": false, "dots": false, "autoplay": false, "autoplayTimeout": 6000, "smartSpeed": 300, "responsive":{ "0" :{ "items": "1" }, "340" :{ "items" : "1" } , "550" :{ "items" : "2" } , "768" :{ "items" : "2" } , "992":{ "items" : "2" }, "1200":{ "items" : "2" }}}'>
+<div class="modal_post_grid">
+<a href="blog-details.html">
+<img src="<?php echo base_url(); ?>modules/uiDesign/assets/images/blog/blog-9-min.png" class="main_img  img-fluid"
+alt="img" />
+<h2 class="tit_ho title_no_a_20">
+Former insures only the marine perils
+</h2>
+</a>
+</div>
+<div class="modal_post_grid">
+<a href="blog-details.html">
+<img src="<?php echo base_url(); ?>modules/uiDesign/assets/images/blog/blog-8-min.png" class="main_img  img-fluid"
+alt="img" />
+<h2 class="tit_ho title_no_a_20">
+Insurance covers risk of fire absence
+</h2>
+</a>
+</div>
+<div class="modal_post_grid">
+<a href="blog-details.html">
+<img src="<?php echo base_url(); ?>modules/uiDesign/assets/images/blog/blog-7-min.png" class="main_img  img-fluid"
+alt="img" />
+<h2 class="tit_ho title_no_a_20">Erving the interests of our clients
+</h2>
+</a>
+</div>
+<div class="modal_post_grid">
+<a href="blog-details.html">
+<img src="<?php echo base_url(); ?>modules/uiDesign/assets/images/blog/blog-6.png" class="main_img  img-fluid"
+alt="img" />
+<h2 class="tit_ho title_no_a_20">
+Guide To Audio Visual With JavaScript GSA
+</h2>
+</a>
+</div>
+<div class="modal_post_grid">
+<a href="blog-details.html">
+<img src="<?php echo base_url(); ?>modules/uiDesign/assets/images/blog/blog-5.png" class="main_img  img-fluid"
+alt="img" />
+<h2 class="tit_ho title_no_a_20">
+Building Web Layouts For Dual Foldable
+Devices
+</h2>
+</a>
+</div>
+
+</div>
+</div>
+</div>
+<h2 class="title_no_a_28"> Need Any Help? Or Looking For an Agent</h2>
+<div class="contact_panel">
+<div class="c_pan">
+<a href="tel:9806071234">
+<i class="fi-rr-phone-call"></i> 9806071234 </a>
+</div>
+<div class="c_pan">
+<a href="mailto:sendmail@example.com">
+<i class="fi-rr-envelope"></i> sendmail@example.com </a>
+</div>
+<div class="c_pan">
+<a class="wk_hours">
+<i class="fi-rr-time-check"></i> Working Hours : <span> Sun-monday,
+09am-5pm </span> </a>
+</div>
+</div>
+<div class="c_pan">
+© 2022 Vankine. All Rights Reserved.
+</div>
+</div>
+
+
+</div>
+</div>
+</div>
+<!----==============option_panel end==============--->
+<!-- Back to top with progress indicator-->
+<div class="prgoress_indicator color_four">
+<svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+<path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+</svg>
+</div>
+<!----=============page_wapper end=============---->
+</div>
+<!----=============page_wapper end=============---->
+
+<!----=================Script================---->
+<script type='text/javascript' src='<?php echo base_url(); ?>modules/uiDesign/assets/js/plugins/jquery.min.js'></script>
+<script type='text/javascript' src='<?php echo base_url(); ?>modules/uiDesign/assets/js/plugins/bootstrap.bundle.min.js'></script>
+<script type='text/javascript' src='<?php echo base_url(); ?>modules/uiDesign/assets/js/plugins/appear.js'></script>
+<script type='text/javascript' src='<?php echo base_url(); ?>modules/uiDesign/assets/js/plugins/TweenMax.min.js'></script>
+<script type='text/javascript' src='<?php echo base_url(); ?>modules/uiDesign/assets/js/plugins/waypoints.js'></script>
+<script type='text/javascript' src='<?php echo base_url(); ?>modules/uiDesign/assets/js/plugins/select.min.js'></script>
+<script type='text/javascript' src='<?php echo base_url(); ?>modules/uiDesign/assets/js/plugins/jquery.fancybox.js'></script>
+<script type='text/javascript' src='<?php echo base_url(); ?>modules/uiDesign/assets/js/plugins/isotope.min.js'></script>
+<script type='text/javascript' src='<?php echo base_url(); ?>modules/uiDesign/assets/js/plugins/slick.js'></script>
+<script type='text/javascript' src='<?php echo base_url(); ?>modules/uiDesign/assets/js/plugins/owl.js'></script>
+<script type='text/javascript' src='<?php echo base_url(); ?>modules/uiDesign/assets/js/plugins/sharer.js'></script>
+<script type='text/javascript' src='<?php echo base_url(); ?>modules/uiDesign/assets/js/plugins/rangeslider.min.js'></script>
+<script type='text/javascript' src='<?php echo base_url(); ?>modules/uiDesign/assets/js/plugins/counterup.js'></script>
+<script type='text/javascript' src='<?php echo base_url(); ?>modules/uiDesign/assets/js/plugins/slick.js'></script>
+<script type='text/javascript' src='<?php echo base_url(); ?>modules/uiDesign/include/validator.min.js'></script>
+<script type='text/javascript' src='<?php echo base_url(); ?>modules/uiDesign/include/contact.js'></script>
+<!-- map script -->
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA-CE0deH3Jhj6GN4YvdCFZS7DpbXexzGU"></script>
+<script src="<?php echo base_url(); ?>modules/uiDesign/assets/js/gmaps.js"></script>
+<script src="<?php echo base_url(); ?>modules/uiDesign/assets/js/map-helper.js"></script>
+<!-- main-js -->
+<script type='text/javascript' src='<?php echo base_url(); ?>modules/uiDesign/assets/js/main.js'></script> 
+<!----=================Script================---->
+</body>
+
+</html>
---
+++ application/views/master_content/ui_navbar.php
@@ -0,0 +1,396 @@
+
+<body class="theme-vankine scrollbarcolor s_color_four">
+<!----=============page_wapper=============---->
+<div id="page" class="page_wapper hfeed site">
+<!----=============page_wapper=============---->
+<!----=============Preloader=============---->
+<!--  <div class="loader-wrap color_four">
+<div class="preloader">
+<div class="preloader-close"><i class="fi-rr-cross"></i></div>
+</div>
+<div class="layer"> </div>
+<div class="animation-preloader">
+<div class="spinner"></div>
+</div>
+</div> -->
+<!----=============Preloader end=============---->
+
+<!----=============Header=============---->
+<div class="header_area " id="header_contents">
+<header class="header-area header-style-1 htype_three position_absolute bg_transparent">
+<div class="main_header_content default_header">
+<div class="large-container">
+<div class="row align-items-center">
+<div class="col-xl-2 col-lg-12 col-md-5 col-sm-5 col-xs-5">
+<div class="logobox">
+<a href="index.html" class="logo navbar-brand">
+<img src="<?php echo base_url(); ?>modules/uiDesign/assets/images/Logo-green.png" alt="Vankine" class="logo_default">
+</a>
+</div>
+</div>
+<div class="col-xl-8 col-lg-9 col-md-0 col-sm-0 col-xs-0 column_menu">
+<div class="navbar_content d-flex align-items-center">
+<ul class="navbar_nav">
+<li
+class="menu-item nav-item menu-item-has-children dropdown mennucolumn_full-six">
+<a href="index.html" class="nav_link">
+<span class="text-link"> Home </span>
+</a>
+<ul class="sub_menu">
+<li class="menu-item nav-item vankine_menu_image">
+<a href="index.html" class="nav_link image_big_on_menu_two">
+<span class="img-link">
+<img src="<?php echo base_url(); ?>modules/uiDesign/assets/images/demo-1-1-min-scaled.jpg"
+alt="icon" class="menu_img">
+</span>
+<span class="text-link"> Home 1 </span>
+</a>
+</li>
+<li class="menu-item vankine_menu_image">
+<a href="home-2.html" class="nav_link image_big_on_menu_two">
+<span class="img-link">
+<img src="<?php echo base_url(); ?>modules/uiDesign/assets/images/demo-1-2-min-scaled.jpg"
+alt="icon" class="menu_img">
+</span>
+<span class="text-link"> Home 2 </span>
+</a>
+</li>
+<li class="menu-item nav-item vankine_menu_image">
+<a href="home-3.html" class="nav_link image_big_on_menu_two">
+<span class="img-link">
+<img src="<?php echo base_url(); ?>modules/uiDesign/assets/images/demo-1-3-min-scaled.jpg"
+alt="icon" class="menu_img">
+</span>
+<span class="text-link"> Home 3 </span>
+</a>
+</li>
+<li class="menu-item nav-item vankine_menu_image">
+<a href="home-4.html" class="nav_link image_big_on_menu_two">
+<span class="img-link">
+<img src="<?php echo base_url(); ?>modules/uiDesign/assets/images/demo-1-4-min-scaled.jpg"
+alt="icon" class="menu_img">
+</span>
+<span class="text-link"> Home 4 </span>
+</a>
+</li>
+<li class="menu-item  nav-item vankine_menu_image">
+<a href="home-5.html" class="nav_link image_big_on_menu_two">
+<span class="img-link">
+<img src="<?php echo base_url(); ?>modules/uiDesign/assets/images/demo-1-5-min-scaled.jpg"
+alt="icon" class="menu_img">
+</span>
+<span class="text-link"> Home 5 </span>
+</a>
+</li>
+<li class="menu-item   nav-item vankine_menu_image">
+<a href="home-6.html" class="nav_link image_big_on_menu_two">
+<span class="img-link">
+<img src="<?php echo base_url(); ?>modules/uiDesign/assets/images/demo-1-6-min-scaled.jpg"
+alt="icon" class="menu_img">
+</span>
+<span class="text-link"> Home 6 </span>
+</a>
+</li>
+</ul>
+
+</li>
+<li
+class="menu-item  menu-item-has-children dropdown  mennucolumn_ nav-item">
+<a href="#" class="nav_link">
+<span class="text-link">
+Pages
+</span>
+</a>
+<ul class="sub_menu">
+<li class="menu-item nav-item">
+<a href="about-us.html" class="nav_link">
+<span class="text-link"> About
+Us
+</span>
+</a>
+</li>
+<li class="menu-item nav-item">
+<a href="faqs.html" class="nav_link">
+<span class="text-link">
+Faq’s
+</span>
+</a>
+</li>
+<li class="menu-item   nav-item">
+<a href="pricing.html" class="nav_link">
+<span class="text-link">
+Pricing
+</span>
+</a>
+</li>
+<li class="menu-item   nav-item">
+<a href="team.html" class="nav_link">
+<span class="text-link">
+Team
+</span>
+</a>
+</li>
+<li class="menu-item   nav-item">
+<a href="team-detail.html" class="nav_link">
+<span class="text-link"> Team
+Detail
+</span>
+</a>
+</li>
+</ul>
+
+</li>
+<li class="menu-item menu-item-has-children dropdown  nav-item">
+<a href="#" class="nav_link">
+<span class="text-link">
+Services
+</span>
+</a>
+<ul class="sub_menu">
+<li class="menu-item nav-item">
+<a href="service.html" class="nav_link">
+<span class="text-link">
+Service Style 1
+</span>
+</a>
+</li>
+<li class="menu-item nav-item">
+<a href="service-style-2.html" class="nav_link">
+<span class="text-link">
+Service Style 2
+</span>
+</a>
+</li>
+<li class="menu-item nav-item">
+<a href="service-style-3.html" class="nav_link">
+<span class="text-link">
+Service Style 3
+</span>
+</a>
+</li>
+<li class="menu-item nav-item">
+<a href="service-style-4.html" class="nav_link">
+<span class="text-link">
+Service Style 4
+</span>
+</a>
+</li>
+<li class="menu-item nav-item">
+<a href="service-style-5.html" class="nav_link">
+<span class="text-link">
+Service Style 5
+</span>
+</a>
+</li>
+<li class="menu-item nav-item">
+<a href="service-style-6.html" class="nav_link">
+<span class="text-link">
+Service Style 6
+</span>
+</a>
+</li>
+<li class="menu-item nav-item">
+<a href="service-details.html" class="nav_link">
+<span class="text-link">
+Service Details
+</span>
+</a>
+</li>
+</ul>
+
+</li>
+<li
+class="menu-item  menu-item-has-children dropdown  mennucolumn_  nav-item">
+<a href="#" class="nav_link">
+<span class="text-link">
+Portfolio
+</span>
+</a>
+<ul class="sub_menu">
+<li class="menu-item nav-item">
+<a href="portfolio-style-1.html" class="nav_link">
+<span class="text-link">
+Portfolio Style 1
+</span>
+</a>
+</li>
+<li class="menu-item nav-item">
+<a href="portfolio-style-2.html" class="nav_link">
+<span class="text-link">
+Portfolio Style 2
+</span>
+</a>
+</li>
+<li class="menu-item nav-item">
+<a href="portfolio-details.html" class="nav_link">
+<span class="text-link">
+Portfolio Details
+</span>
+</a>
+</li>
+</ul>
+</li>
+<li
+class="menu-item  menu-item-has-children dropdown  mennucolumn_two   nav-item">
+<a href="blog.html" class="nav_link">
+<span class="text-link">
+Blog
+</span>
+</a>
+<ul class="sub_menu">
+<li class="menu-item menu-item-has-children dropdown  nav-item">
+<a href="#" class="nav_link">
+<span class="text-link">
+Blog Grid
+</span>
+</a>
+<ul class="sub_menu">
+<li class="menu-item  nav-item">
+<a href="blog.html" class="nav_link">
+<span class="text-link">
+Blog Default
+</span>
+</a>
+</li>
+<li class="menu-item  nav-item">
+<a href="blog-style-1.html" class="nav_link">
+<span class="text-link">
+Blog Style 1
+</span>
+</a>
+</li>
+<li class="menu-item  nav-item">
+<a href="blog-style-2.html" class="nav_link">
+<span class="text-link">
+Blog Style 2
+</span>
+</a>
+</li>
+</ul>
+</li>
+<li class="menu-item menu-item-has-children dropdown  nav-item">
+<a href="#" class="nav_link"><span class="text-link"> Blog
+Details </span></a>
+<ul class="sub_menu">
+<li class="menu-item   nav-item">
+<a href="blog-details.html" class="nav_link">
+<span class="text-link">
+No Sidebar
+</span>
+</a>
+</li>
+<li class="menu-item  nav-item">
+<a href="blog-details-left-sidebar.html"
+class="nav_link">
+<span class="text-link">
+Left Sidebar
+</span>
+</a>
+</li>
+<li class="menu-item nav-item">
+<a href="blog-details-right-sidebar.html"
+class="nav_link">
+<span class="text-link">
+Right Sidebar
+</span>
+</a>
+</li>
+</ul>
+</li>
+</ul>
+</li>
+<li
+class="menu-item  menu-item-has-children dropdown  mennucolumn_  nav-item">
+<a href="shop.html" class="nav_link">
+<span class="text-link">
+Shop
+</span>
+</a>
+<ul class="sub_menu">
+<li class="menu-item nav-item">
+<a href="shop.html" class="nav_link">
+<span class="text-link">
+Products
+</span>
+</a>
+</li>
+<li class="menu-item nav-item">
+<a href="shop-details.html" class="nav_link">
+<span class="text-link">
+Product Details
+</span>
+</a>
+</li>
+<li class="menu-item nav-item">
+<a href="my-account.html" class="nav_link">
+<span class="text-link">
+My account
+</span>
+</a>
+</li>
+<li class="menu-item nav-item">
+<a href="cart.html" class="nav_link">
+<span class="text-link">
+Cart
+</span>
+</a>
+</li>
+<li class="menu-item nav-item">
+<a href="checkout.html" class="nav_link">
+<span class="text-link">
+Checkout
+</span>
+</a>
+</li>
+</ul>
+</li>
+<li class="menu-item  nav-item">
+<a href="contact.html" class="nav_link">
+<span class="text-link">
+Contact
+</span>
+</a>
+</li>
+</ul>
+<div class="search search-toggler">
+<i class="fi-rs-search"></i>
+</div>
+</div>
+</div>
+<div class="col-xl-2 col-lg-3 col-md-7 col-sm-7 col-xs-7 nav_tog_column">
+<div class="d-flex right_content align-items-center">
+<!--menu icon-->
+<div class="navbar_togglers hamburger_menu">
+<span class="line"></span>
+<span class="line"></span>
+<span class="line"></span>
+</div>
+<!--menu icon-->
+<div class="button">
+<a href="#" class="theme_btn">
+Get A Quote
+<svg width="12" height="12" viewBox="0 0 12 12" fill="none"
+xmlns="http://www.w3.org/2000/svg">
+<path d="M1 11L11 1M11 1H3.5M11 1V8.5" stroke="white" stroke-width="1.5"
+stroke-linecap="round" stroke-linejoin="round"></path>
+</svg>
+</a>
+</div>
+<div class="contact-toggler">
+<svg width="18" height="19" viewBox="0 0 18 19" fill="none"
+xmlns="http://www.w3.org/2000/svg">
+<path
+d="M0.75 3.25C0.75 2.65326 0.987053 2.08097 1.40901 1.65901C1.83097 1.23705 2.40326 1 3 1H5.25C5.84674 1 6.41903 1.23705 6.84099 1.65901C7.26295 2.08097 7.5 2.65326 7.5 3.25V5.5C7.5 6.09674 7.26295 6.66903 6.84099 7.09099C6.41903 7.51295 5.84674 7.75 5.25 7.75H3C2.40326 7.75 1.83097 7.51295 1.40901 7.09099C0.987053 6.66903 0.75 6.09674 0.75 5.5V3.25ZM0.75 13C0.75 12.4033 0.987053 11.831 1.40901 11.409C1.83097 10.9871 2.40326 10.75 3 10.75H5.25C5.84674 10.75 6.41903 10.9871 6.84099 11.409C7.26295 11.831 7.5 12.4033 7.5 13V15.25C7.5 15.8467 7.26295 16.419 6.84099 16.841C6.41903 17.2629 5.84674 17.5 5.25 17.5H3C2.40326 17.5 1.83097 17.2629 1.40901 16.841C0.987053 16.419 0.75 15.8467 0.75 15.25V13ZM10.5 3.25C10.5 2.65326 10.7371 2.08097 11.159 1.65901C11.581 1.23705 12.1533 1 12.75 1H15C15.5967 1 16.169 1.23705 16.591 1.65901C17.0129 2.08097 17.25 2.65326 17.25 3.25V5.5C17.25 6.09674 17.0129 6.66903 16.591 7.09099C16.169 7.51295 15.5967 7.75 15 7.75H12.75C12.1533 7.75 11.581 7.51295 11.159 7.09099C10.7371 6.66903 10.5 6.09674 10.5 5.5V3.25ZM10.5 13C10.5 12.4033 10.7371 11.831 11.159 11.409C11.581 10.9871 12.1533 10.75 12.75 10.75H15C15.5967 10.75 16.169 10.9871 16.591 11.409C17.0129 11.831 17.25 12.4033 17.25 13V15.25C17.25 15.8467 17.0129 16.419 16.591 16.841C16.169 17.2629 15.5967 17.5 15 17.5H12.75C12.1533 17.5 11.581 17.2629 11.159 16.841C10.7371 16.419 10.5 15.8467 10.5 15.25V13Z"
+stroke="#0F3567" stroke-width="1.5" stroke-linecap="round"
+stroke-linejoin="round">
+</path>
+</svg>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</header>
+</div>
+<!----=============Header end=============---->
